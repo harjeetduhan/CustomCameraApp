f0d58accf649fe60ebe1e8d9234690ef690930f8
abcd <abcd@bliss-its-mac-pro.local>
2017-05-22 10:56:13 -0700

    Initial Commit

---
+++ CustomCameraApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCameraApp
+//
+//  Created by bliss on 22/05/17.
+//  Copyright © 2017 bliss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomCameraApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCameraApp
+//
+//  Created by bliss on 22/05/17.
+//  Copyright © 2017 bliss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
